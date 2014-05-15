3a692110e4a031b637ea8ae92b81ac48b5757edb
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-15 15:32:46 -0700

    Switching Image Function not working

--- js/practice.js
+++ js/practice.js
@@ -5,3 +5,12 @@
 	function changeText() {
 		document.getElementById("ct").innerHTML = "Gone Fishing.";
 	}
+
+	function switchImage() {
+		var image = document.getElementById('switchImage');
+		if (image.src.match('images/cat.png')) {
+			image.src = "images/beachball.png";
+		} else {
+			image.src = "images/cat.png";
+		}
+	}
