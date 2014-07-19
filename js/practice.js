1a56fb2ea19d60dcbb08dd626396ffcc36c63ce7
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-18 22:42:51 -0700

    Tried to used Argument object, turned into splicing array into sum

--- js/practice.js
+++ js/practice.js
@@ -439,3 +439,14 @@
 		}
 		x.innerHTML = text;
 	}
+
+	function showArgument() {
+		var x = $("input_arguments").value;
+		var arr = x.split(",");
+		var y = $("text_arguments");
+		var sum = 0;
+		for (var i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		y.innerHTML = sum;
+	}
