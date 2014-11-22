d4f0815eac788977c353d00318b0be4734c1f6ce
sophalkim <sophal.kim.work@gmail.com>
2014-11-22 08:46:20 -0800

    Using javascript instead of jQuery

---
+++ js/practice2.js
@@ -0,0 +1,28 @@
+function showClock() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	var x = $("clock");
+	x.innerHTML = h + ":" + m + ":" + s;
+	var t = setTimeout(function() {
+		showClock()
+	}, 500);
+}
+
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+function displayText() {
+	document.getElementById("demo").innerHTML = Date();
+}
