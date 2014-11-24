7bf6251106d061d7733d9fa8c6f47ea0148a44c9
sophalkim <sophal.kim.work@gmail.com>
2014-11-24 07:39:00 -0800

    Added Switch Image Function

--- js/practice2.js
+++ js/practice2.js
@@ -26,3 +26,16 @@ function $(id) {
 function displayText() {
 	document.getElementById("demo").innerHTML = Date();
 }
+
+function changeText() {
+	document.getElementById("ct").innerHTML = "Gone Fishing.";
+}
+
+function switchImage() {
+	var image = document.getElementById('switchimage');
+	if (image.src.match('images/cat.png')) {
+		image.src = "images/beachball.png";
+	} else {
+		image.src = "images/cat.png"
+	}
+}
